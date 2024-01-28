252dcb27ad052955d051fd84f59cade18cfbeb31
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-01-28 03:50:05 +0000

    #

---
+++ q1.jsx
@@ -0,0 +1,8 @@
+function evenodd(number){
+   var result= number % 2 == 0 ? "even": "odd";
+   return result;
+}
+
+//The ternary operator in JavaScript is a concise way to write a conditional expression. It has the following syntax:
+//If the condition is true, the expression before the : is evaluated and returned.
+//If the condition is false, the expression after the : is evaluated and returned.
