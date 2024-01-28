f328ff26055dfd115989a918b1b8e9b62a80b1fa
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-01-28 18:16:30 +0000

    #

---
+++ q2.jsx
@@ -0,0 +1,11 @@
+// Working Directory: This is where you make changes to your files. It's your local sandbox where you modify, add, or delete files.
+
+// Staging Area (Index): After making changes in the working directory, you stage them in the index. Staging is like preparing a snapshot of the changes you want to commit. This allows you to selectively include or exclude changes before committing them.
+
+// Repository: The repository is where Git permanently stores the committed changes. It includes a complete history of all changes, branches, and tags.
+
+// The basic Git workflow involves:
+
+// Modify files in the working directory.
+// Stage the changes using git add to move them to the staging area.
+// Commit the changes using git commit to save them in the repository.
